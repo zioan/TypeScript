6cbc2dd8bedb43de932498d3599dd56bf37da2f1
zioan <zaharia.ioan@gmail.com>
2022-05-18 12:04:27 +0200

    TS compiler

---
+++ compiler/app.ts
@@ -0,0 +1,10 @@
+const userName = 'Ioan';
+console.log('from app.ts file...');
+
+//To watch a single TS file for changes
+// tsc app.ts -w
+
+//To watch an entire TS project for changes
+// tsc --init (only one time)
+// tsc -w
+// npm start (if a live server is installed -> see package.json)
