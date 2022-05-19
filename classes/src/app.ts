88449f95c2b6942f69d12b40bc63357f86984954
zioan <zaharia.ioan@gmail.com>
2022-05-19 10:38:35 +0200

    more advanced classes

--- classes/src/app.ts
+++ classes/src/app.ts
@@ -1,17 +1,26 @@
 class Department {
+  static fiscalYear = 2022; //make it available outside the class as global property
   //private id: string
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = []; //protected is like private but allows inheritance
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id
     // this.name = n;
+
+    //'this.' does not work on static props or methods inside class
+    console.log(Department.fiscalYear);
+  }
+
+  //make it available outside the class as global method
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   //mehtods
   // describe() {
   describe(this: Department) {
-    console.log(`Department ${this.id}: ${this.name}`);
+    // console.log(`Department ${this.id}: ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -25,17 +34,88 @@ class Department {
   }
 }
 
-const accounting = new Department('id1', 'Accounting');
-console.log(accounting); //an object
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
-accounting.describe(); //calling 'describe' method inherited from the class
+//calling a static method
+const employee1 = Department.createEmployee('Sara');
+console.log(employee1);
 
-// //create an object that inherit a class method
+//calling a static property
+console.log(Department.fiscalYear);
+
+const it = new ITDepartment('id1', ['Tom']);
+console.log(it); //an object
+it.describe(); //calling 'describe' method inherited from the class
+it.addEmployee('Bob');
+it.addEmployee('John');
+it.printEmployeeInformation();
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  //getter
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  //setter
+  set mostRecentReport(value: string) {
+    if (!value) {
+      // return;
+      throw new Error('Please pass in a value!');
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  describe() {
+    console.log('Accountind Department -ID', this.id);
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.mostRecentReport = 'New report data...'; //set (setter)
+accounting.describe();
+
+// console.log(accounting.mostRecentReport);
+accounting.addReport('Some text...');
+console.log(accounting.mostRecentReport); //get (getter)
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Tom');
+accounting.printEmployeeInformation();
+
+accounting.printReports();
+
 // const accountingCopy = { name: 'test', describe: accounting.describe };
 // accountingCopy.describe();
 // console.log(typeof accountingCopy);
-
-accounting.addEmployee('Bob');
-accounting.addEmployee('John');
-
-accounting.printEmployeeInformation();
